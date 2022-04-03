e5a64a4bc7c098416b9d08371645e2685f937f13
Monali-W <wasekarmonaligmail.com>
2022-04-03 19:41:45 -0400

    contact page imported to App Component

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NavbarComponent from "./components/NavbarComponent";
 import About from "./components/About";
+import Project from "./components/Projects";
+import Contact from "./components/Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <NavbarComponent />
-      <About />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <NavbarComponent />
+        <About />
+        <Project />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
