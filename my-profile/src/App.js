dcc0923afb1f3fe8682d755d9c54acbddb49389a
Monali-W <wasekarmonaligmail.com>
2022-04-08 15:16:38 -0400

    navbar Added

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import NavbarComponent from "./components/NavbarComponent";
+import NavbarComponent from "./components/NavbarComponent";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -10,8 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        {/* <NavbarComponent /> */}
+        {/* <Header /> */}
+        <NavbarComponent />
         <About />
         <Projects />
         <Contact />
