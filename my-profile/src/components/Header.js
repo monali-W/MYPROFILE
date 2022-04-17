4b82172b34872b1dad36753942fa94fd6e312556
Monali-W <wasekarmonaligmail.com>
2022-04-17 10:16:52 -0500

    Added name

--- my-profile/src/components/Header.js
+++ my-profile/src/components/Header.js
@@ -8,8 +8,22 @@ class Header extends Component {
           <div className="container">
             <div className="row">
               <div className="col name align-item-center">
-                <h2>MONALI WASEKAR</h2>
-                <h3> UI Developer</h3>
+                <h1
+                  className="mb-4"
+                  data-aos="fade-right"
+                  data-aos-delay="50"
+                  data-aos-duration="1000"
+                >
+                  MONALI WASEKAR
+                </h1>
+                <h4
+                  data-aos="fade-left"
+                  data-aos-delay="50"
+                  data-aos-duration="1000"
+                >
+                  {" "}
+                  FRONTEND DEVELOPER
+                </h4>
               </div>
             </div>
           </div>
